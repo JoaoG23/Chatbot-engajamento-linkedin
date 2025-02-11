b2de6d65236f2f9541e9f6f024ef4ba9506490ab
Joao_Guilherme2 <80895578+JoaoG23@users.noreply.github.com>
2025-02-11 18:28:49 -0300

    :recycle: refactor: Remove commented-out functions in comment handling module

--- get_posts/comment_in_post_by_limit/comment_in_post_by_limit.py
+++ get_posts/comment_in_post_by_limit/comment_in_post_by_limit.py
@@ -13,47 +13,6 @@ from utils.remove_linebreak_text.remove_linebreak_text import remove_linebreak_t
 
 SCROLL_PAUSE_TIME = 20
 
-# def give_like_in_post(post):
-#     sleep(3)
-#     like_button = WebDriverWait(post, SCROLL_PAUSE_TIME).until(
-#         EC.element_to_be_clickable((By.XPATH, './/*[@aria-label="Reagir com gostei"]'))
-#     )
-#     like_button.click()
-#     sleep(3)
-
-# def get_content_in_the_post(post):
-#     try:
-#         more_content_button = WebDriverWait(post, SCROLL_PAUSE_TIME).until(
-#             EC.element_to_be_clickable((By.XPATH, f'.//*[contains(text(), "…mais")]'))
-#         )
-#         more_content_button.click()
-#     except:
-#         pass 
-    
-#     sleep(3)
-#     text_of_post = WebDriverWait(post, SCROLL_PAUSE_TIME).until(
-#         EC.visibility_of_element_located((By.CLASS_NAME, 'feed-shared-update-v2__description'))
-#     )
-#     return text_of_post.text
-
-# def fill_comment_input_and_send(driver, post, content_post, quantity_posts_comments):
-#     comment_button = WebDriverWait(post, SCROLL_PAUSE_TIME).until(
-#         EC.element_to_be_clickable((By.XPATH, './/*[@aria-label="Comentar"]'))
-#     )
-#     comment_button.click()
-    
-#     sleep(5)
-#     comment_inputs = WebDriverWait(post, SCROLL_PAUSE_TIME).until(
-#         EC.presence_of_all_elements_located((By.XPATH, '//*[@aria-placeholder="Adicionar comentário"]'))
-#     )
-#     comment_inputs[quantity_posts_comments].send_keys(content_post)
-    
-#     sleep(2)
-#     send_button = driver.find_element(By.CSS_SELECTOR, '.comments-comment-box__submit-button--cr.artdeco-button.artdeco-button--1.artdeco-button--primary.ember-view')
-#     send_button.click()
-#     sleep(5)
-
-
 def comment_in_post_by_limit(driver, limit_comments):
     sleep(4)
     container_posts = WebDriverWait(driver, SCROLL_PAUSE_TIME).until(
@@ -71,8 +30,8 @@ def comment_in_post_by_limit(driver, limit_comments):
         
         content_in_post = get_content_in_the_post(post)
         comment_created = create_comment_based_post(content_in_post)
-        comment_without_emoji = remove_linebreak_text(comment_created)
-        comment_without_emoji_and_linebreak = remove_emojis_text(comment_without_emoji)
+        comment_without_linebreak = remove_linebreak_text(comment_created)
+        comment_without_emoji_and_linebreak = remove_emojis_text(comment_without_linebreak)
     
         fill_comment_input_and_send(driver, post, comment_without_emoji_and_linebreak,i)
         
