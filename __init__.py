cde2fd61e73b09d1f6de8da83729e4a187da62fc
Joao_Guilherme2 <80895578+JoaoG23@users.noreply.github.com>
2025-05-29 20:04:22 -0300

    :sparkles: feat: Add search in homepage of linkedin

--- __init__.py
+++ __init__.py
@@ -4,14 +4,10 @@ import traceback
 
 from dotenv import load_dotenv
 
-from httpcore import TimeoutException
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
-from selenium.common.exceptions import NoSuchElementException
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
-
-from selenium.common.exceptions import InvalidSelectorException
 
 from get_posts.do_login.do_login import do_login
 from get_posts.search_posts_and_comment.search_posts_and_comment import search_posts_and_comment
@@ -38,8 +34,6 @@ if __name__ == '__main__':
         print("Esperamos que você aproveite!")
         print("e Um beijo na sua bundinha!....😁")
         
-        search_information_input = input("Digite o que deseja buscar no LinkedIn:  ")
-
         driver.implicitly_wait(15)
         driver.maximize_window()
         
@@ -49,16 +43,10 @@ if __name__ == '__main__':
         }
         do_login(driver, user_login)
         
-        search_posts_and_comment(driver, search_information_input)
+        search_posts_and_comment(driver)
    
     except WebDriverException as e:
         write_to_log(f"WebDriverException: {traceback.format_exc()}", type='error')
-    except NoSuchElementException as e:
-        write_to_log(f"NoSuchElementException: {traceback.format_exc()}", type='error')
-    except InvalidSelectorException as e:
-        write_to_log(f"InvalidSelectorException: {traceback.format_exc()}", type='error')
-    except TimeoutException as e:
-        write_to_log(f"TimeoutException: {traceback.format_exc()}", type='error')
     except Exception as e:
         write_to_log(f"Exception: {traceback.format_exc()}", type='error')
     finally:
